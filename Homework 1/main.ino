ece924931f1df3a91cf1d58ace486d21715f9cfe
Rusu Andrei-Cristian <qfl1ck32@yahoo.com>
2021-10-22 18:43:09 +0300

    (move setup() near loop())

--- Homework 1/main.ino
+++ Homework 1/main.ino
@@ -11,14 +11,6 @@ const int maxAnalogWriteValue = 255;
 
 const int maxAnalogReadValue = 1023;
 
-void setup() {
-  for (unsigned int i = 0; i < numberOfColors; ++i) {
-    pinMode(potPins[i], INPUT);
-    pinMode(ledPins[i], OUTPUT);
-  }
-
-   Serial.begin(9600);
-}
 
 float *getPotValues() {
   float *potValues = (float *) malloc(numberOfColors * sizeof(float));
@@ -45,6 +37,15 @@ void writePotValuesToLedPins(float *potValues) {
     
     analogWrite(ledPins[i], brightness);
   }
+}
+
+void setup() {
+  for (unsigned int i = 0; i < numberOfColors; ++i) {
+    pinMode(potPins[i], INPUT);
+    pinMode(ledPins[i], OUTPUT);
+  }
+
+   Serial.begin(9600);
 }
 
 void loop() {
