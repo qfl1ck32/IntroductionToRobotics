7489c5fed7dba4663fa5f6a5ebb641fe8d854c4b
Rusu Andrei-Cristian <qfl1ck32@yahoo.com>
2021-10-22 17:59:38 +0300

    Remove prettier

--- Homework 1/main.ino
+++ Homework 1/main.ino
@@ -2,37 +2,31 @@
 
 const int numberOfColors = 3;
 
-const int potPins[numberOfColors] = {A0, A1, A2};
+const int potPins[numberOfColors] = { A0, A1, A2 };
 
-const int ledPins[numberOfColors] = {3, 5, 6};
+const int ledPins[numberOfColors] = { 3, 5, 6 };
 
-void setup()
-{
-  for (unsigned int i = 0; i < numberOfColors; ++i)
-  {
+void setup() {
+  for (unsigned int i = 0; i < numberOfColors; ++i) {
     pinMode(potPins[i], INPUT);
     pinMode(ledPins[i], OUTPUT);
   }
 
-  Serial.begin(9600);
+   Serial.begin(9600);
 }
 
-float *getPotValues()
-{
-  float *potValues = (float *)malloc(numberOfColors * sizeof(float));
-
-  for (unsigned int i = 0; i < numberOfColors; ++i)
-  {
+float *getPotValues() {
+  float *potValues = (float *) malloc(numberOfColors * sizeof(float));
+ 
+  for (unsigned int i = 0; i < numberOfColors; ++i) {
     potValues[i] = analogRead(potPins[i]);
   }
 
   return potValues;
 }
 
-void printPotValues(float *potValues)
-{
-  for (unsigned int i = 0; i < numberOfColors - 1; ++i)
-  {
+void printPotValues(float *potValues) {
+  for (unsigned int i = 0; i < numberOfColors - 1; ++i) {
     Serial.print(potValues[i]);
     Serial.print(", ");
   }
@@ -40,16 +34,13 @@ void printPotValues(float *potValues)
   Serial.println(potValues[numberOfColors - 1]);
 }
 
-void writePotValuesToLedPins(float *potValues)
-{
-  for (unsigned int i = 0; i < numberOfColors; ++i)
-  {
+void writePotValuesToLedPins(float *potValues) {
+  for (unsigned int i = 0; i < numberOfColors; ++i) {
     analogWrite(ledPins[i], potValues[i] / 4.0);
   }
 }
 
-void loop()
-{
+void loop() {
   float *potValues = getPotValues();
 
   printPotValues(potValues);
