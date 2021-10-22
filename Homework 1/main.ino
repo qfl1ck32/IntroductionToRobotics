b38108f6c39b05cafb97f5082ff07f6eda256f63
Rusu Andrei-Cristian <qfl1ck32@yahoo.com>
2021-10-22 17:58:23 +0300

    Add Homework 1

---
+++ Homework 1/main.ino
@@ -0,0 +1,59 @@
+// Convention: the arrays are meant to store { R, G, B }-related values (i.e. potPins[0] = the pin of the potentiometer associated with the red light)
+
+const int numberOfColors = 3;
+
+const int potPins[numberOfColors] = {A0, A1, A2};
+
+const int ledPins[numberOfColors] = {3, 5, 6};
+
+void setup()
+{
+  for (unsigned int i = 0; i < numberOfColors; ++i)
+  {
+    pinMode(potPins[i], INPUT);
+    pinMode(ledPins[i], OUTPUT);
+  }
+
+  Serial.begin(9600);
+}
+
+float *getPotValues()
+{
+  float *potValues = (float *)malloc(numberOfColors * sizeof(float));
+
+  for (unsigned int i = 0; i < numberOfColors; ++i)
+  {
+    potValues[i] = analogRead(potPins[i]);
+  }
+
+  return potValues;
+}
+
+void printPotValues(float *potValues)
+{
+  for (unsigned int i = 0; i < numberOfColors - 1; ++i)
+  {
+    Serial.print(potValues[i]);
+    Serial.print(", ");
+  }
+
+  Serial.println(potValues[numberOfColors - 1]);
+}
+
+void writePotValuesToLedPins(float *potValues)
+{
+  for (unsigned int i = 0; i < numberOfColors; ++i)
+  {
+    analogWrite(ledPins[i], potValues[i] / 4.0);
+  }
+}
+
+void loop()
+{
+  float *potValues = getPotValues();
+
+  printPotValues(potValues);
+  writePotValuesToLedPins(potValues);
+
+  free(potValues);
+}
